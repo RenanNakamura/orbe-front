0a9fc3c86b99c5640e34738de04fe6e81f74890a
RenanNakamura <nakamura.renan.j@gmail.com>
2025-10-29 14:37:53 -0300

    - Implementando agents.

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,86 +1,81 @@
 import {Language} from './sk/Language';
 
 export interface LoggedUser {
-    name: string;
-    role: Role;
-    language: Language;
-    plan: PlanEnum;
+  name: string;
+  role: Role;
+  language: Language;
+  plan: PlanEnum;
 }
 
 export interface User {
 
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    status: Status;
-    ddi: string;
-    number: string;
-    language: Language;
-    applicationId: string;
-    phoneNumberId: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  status: Status;
+  ddi: string;
+  number: string;
+  language: Language;
+  applicationId: string;
+  phoneNumberId: string;
 
 }
 
 export enum Role {
 
-    ADMIN = 'ADMIN',
-    USER = 'USER'
+  ADMIN = 'ADMIN',
+  USER = 'USER'
 
 }
 
 export enum PlanEnum {
 
-    BASIC = 'BASIC',
-    PRO = 'PRO'
+  BASIC = 'BASIC',
+  PRO = 'PRO'
 
 }
 
 export enum Status {
 
-    ACTIVE = 'ACTIVE',
-    INACTIVE = 'INACTIVE'
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE'
 
 }
 
 export interface UserParams {
-    page?: number;
-    size?: number;
-    sort?: string;
-    search?: string;
+  page?: number;
+  size?: number;
+  sort?: string;
+  search?: string;
 }
 
 export interface Onboarding {
-    createdAt: string;
-    updatedAt: string;
-    gateway: string;
-    status: OnboardingStatus;
-    token: string;
-    customerId: string;
-    customerName: string;
-    customerEmail: string;
-    customerPhone: string;
-    plan: Plan;
-    accessAllowedDate: string;
+  createdAt: string;
+  updatedAt: string;
+  gateway: string;
+  status: OnboardingStatus;
+  token: string;
+  customerId: string;
+  customerName: string;
+  customerEmail: string;
+  customerPhone: string;
+  plan: Plan;
+  accessAllowedDate: string;
 }
 
 export enum OnboardingStatus {
-    PENDING = 'PENDING', FINISHED = 'FINISHED'
+  PENDING = 'PENDING', FINISHED = 'FINISHED'
 }
 
 export interface Plan {
-    id: string;
-    plan: string;
-    maxChannelCount: string;
+  id: string;
+  plan: string;
+  maxChannelCount: string;
 }
 
 export interface FinishOnboarding {
-    name: string;
-    phone: string;
-    email: string;
-    language: Language;
-    gateway: string;
-    plan: string;
-    maxChannelCount: number;
-    password: string;
+  name: string;
+  language: Language;
+  password: string;
 }
--- src/app/module/login/onboarding/onboarding.component.ts
+++ src/app/module/login/onboarding/onboarding.component.ts
@@ -1,121 +1,106 @@
 import {Component, OnInit} from '@angular/core';
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
-import {AlertService} from '../../../service/sk/alert.service';
-import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {OnboardingService} from '../../../service/user/onboarding.service';
 import {FinishOnboarding, Onboarding} from '../../../model/User';
-import {UserService} from '../../../service/user/user.service';
 import {Language} from '../../../model/sk/Language';
 import {environment} from '../../../../environments/environment';
 
 declare var grecaptcha: any;
 
 @Component({
-    selector: 'vex-onboarding',
-    templateUrl: './onboarding.component.html',
-    styleUrls: ['./onboarding.component.scss']
+  selector: 'vex-onboarding',
+  templateUrl: './onboarding.component.html',
+  styleUrls: ['./onboarding.component.scss']
 })
 export class OnboardingComponent implements OnInit {
 
-    form = this.fb.group({
-        name: ['', [Validators.required]],
-        phone: ['', [Validators.required]],
-        email: ['', [Validators.required, Validators.email]],
-        language: ['', [Validators.required]],
-        gateway: [''],
-        plan: [''],
-        maxChannelCount: [1],
-        password: ['', [Validators.required]],
-        confirmPassword: ['', Validators.required]
+  form = this.fb.group({
+    name: ['', [Validators.required]],
+    phone: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    language: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validator: this.passwordIsMatch
+  });
+
+  languages = Language;
+  isSuccess = false;
+  token = '';
+  onboardingId = '';
+
+  constructor(private readonly fb: UntypedFormBuilder,
+              private _service: OnboardingService,
+              private readonly activatedRoute: ActivatedRoute,
+              private _router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute
+      .params
+      .subscribe(params => {
+        this.token = params?.['token'];
+        this.onboardingId = params?.['onboardingId'];
+
+        this._service.get(this.onboardingId, this.token)
+          .subscribe(onboarding => this.loadOnboardingOnForm(onboarding));
+      });
+  }
+
+  async onSubmit() {
+    if (this.form.valid) {
+      const user: FinishOnboarding = {
+        name: this.form.get('name').value,
+        language: this.form.get('language').value,
+        password: this.form.get('password').value,
+      };
+
+      grecaptcha.ready(() => {
+        grecaptcha.execute(environment.receptcha.sitekey, {action: 'ONBOARDING'})
+          .then((token) => {
+            this._service.finish(this.onboardingId, this.token, token, user)
+              .subscribe(() => {
+                this.isSuccess = true;
+
+                setTimeout(() => {
+                  this._router.navigate(['/login']);
+                }, 3000);
+              });
+          });
+      });
+    }
+  }
+
+  passwordIsMatch(control: UntypedFormGroup) {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (!confirmPassword?.value) {
+      return;
+    }
+
+    if (password?.value !== confirmPassword?.value) {
+      control.get('confirmPassword').setErrors({passwordNotMatch: true});
+    }
+
+    if (password?.value === confirmPassword?.value) {
+      control.get('confirmPassword').setErrors(null);
+    }
+  }
+
+  private loadOnboardingOnForm(onboarding: Onboarding) {
+    this.form = this.fb.group({
+      name: [onboarding?.customerName || '', [Validators.required]],
+      phone: [{value: onboarding?.customerPhone || '', disabled: true}, [Validators.required]],
+      email: [{value: onboarding?.customerEmail || '', disabled: true}, [Validators.required, Validators.email]],
+      language: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', Validators.required],
     }, {
-        validator: this.passwordIsMatch
+      validator: this.passwordIsMatch
     });
-
-    languages = Language;
-    isSuccess = false;
-    token = '';
-    onboardingId = '';
-
-    constructor(private readonly fb: UntypedFormBuilder,
-                private _service: OnboardingService,
-                private _userService: UserService,
-                private _alertService: AlertService,
-                private readonly activatedRoute: ActivatedRoute,
-                private _router: Router,
-                private _translate: TranslateService) {
-    }
-
-    ngOnInit(): void {
-        this.activatedRoute.params.subscribe(params => {
-            this.token = params?.['token'];
-            this.onboardingId = params?.['onboardingId'];
-
-            this._service.get(this.onboardingId, this.token)
-                .subscribe(onboarding => this.loadOnboardingOnForm(onboarding));
-        });
-    }
-
-    async onSubmit() {
-        if (this.form.valid) {
-            const user: FinishOnboarding = {
-                name: this.form.get('name').value,
-                phone: this.form.get('phone').value,
-                email: this.form.get('email').value,
-                language: this.form.get('language').value,
-                gateway: this.form.get('gateway').value,
-                plan: this.form.get('plan').value,
-                maxChannelCount: this.form.get('maxChannelCount').value,
-                password: this.form.get('password').value,
-            };
-
-            grecaptcha.ready(() => {
-                grecaptcha.execute(environment.receptcha.sitekey, {action: 'ONBOARDING'})
-                    .then((token) => {
-                        this._service.finish(this.onboardingId, this.token, token, user)
-                            .subscribe(() => {
-                                this.isSuccess = true;
-
-                                setTimeout(() => {
-                                    this._router.navigate(['/login']);
-                                }, 3000);
-                            });
-                    });
-            });
-        }
-    }
-
-    passwordIsMatch(control: UntypedFormGroup) {
-        const password = control.get('password');
-        const confirmPassword = control.get('confirmPassword');
-
-        if (!confirmPassword?.value) {
-            return;
-        }
-
-        if (password?.value !== confirmPassword?.value) {
-            control.get('confirmPassword').setErrors({passwordNotMatch: true});
-        }
-
-        if (password?.value === confirmPassword?.value) {
-            control.get('confirmPassword').setErrors(null);
-        }
-    }
-
-    private loadOnboardingOnForm(onboarding: Onboarding) {
-        this.form = this.fb.group({
-            name: [onboarding?.customerName || '', [Validators.required]],
-            phone: [{value: onboarding?.customerPhone || '', disabled: true}, [Validators.required]],
-            email: [{value: onboarding?.customerEmail || '', disabled: true}, [Validators.required, Validators.email]],
-            language: ['', [Validators.required]],
-            gateway: [onboarding?.gateway],
-            plan: [onboarding?.plan?.plan || ''],
-            maxChannelCount: [onboarding?.plan?.maxChannelCount || 1],
-            password: ['', [Validators.required]],
-            confirmPassword: ['', Validators.required],
-        }, {
-            validator: this.passwordIsMatch
-        });
-    }
+  }
 
 }
