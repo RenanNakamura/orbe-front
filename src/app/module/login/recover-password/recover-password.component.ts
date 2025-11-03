a56fe036923aae040da73d1efaba5bb2623991fb
RenanNakamura <nakamura.renan.j@gmail.com>
2025-11-03 12:31:22 -0300

    - Implementando agents recover.

--- src/app/module/login/recover-password/recover-password.component.ts
+++ src/app/module/login/recover-password/recover-password.component.ts
@@ -4,85 +4,83 @@ import {UserService} from '../../../service/user/user.service';
 import {AlertService} from '../../../service/sk/alert.service';
 import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import { environment } from 'src/environments/environment';
+import {environment} from 'src/environments/environment';
 
 declare var grecaptcha: any;
 
 @Component({
-    selector: 'vex-forgot-password',
-    templateUrl: './recover-password.component.html',
-    styleUrls: ['./recover-password.component.scss']
+  selector: 'vex-forgot-password',
+  templateUrl: './recover-password.component.html',
+  styleUrls: ['./recover-password.component.scss']
 })
 export class RecoverPasswordComponent implements OnInit {
 
-    form = this.fb.group({
-        email: ['', [Validators.required, Validators.email]],
-        newPassword: ['', [Validators.required]],
-        confirmNewPassword: ['', Validators.required],
-    }, {
-        validator: this.passwordIsMatch
+  form = this.fb.group({
+    newPassword: ['', [Validators.required]],
+    confirmNewPassword: ['', Validators.required],
+  }, {
+    validator: this.passwordIsMatch
+  });
+
+  isSuccess = false;
+  token = '';
+  recoverPasswordId = '';
+
+  constructor(private readonly fb: UntypedFormBuilder,
+              private _service: UserService,
+              private _alertService: AlertService,
+              private readonly activatedRoute: ActivatedRoute,
+              private _router: Router,
+              private _translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.token = params?.['token'];
+      this.recoverPasswordId = params?.['recoverPasswordId'];
     });
+  }
 
-    isSuccess = false;
-    token = '';
-    recoverPasswordId = '';
+  async onSubmit() {
+    if (this.form.valid) {
+      const recoverPassword = {
+        token: this.token,
+        recoverPasswordId: this.recoverPasswordId,
+        newPassword: this.form.get('newPassword').value
+      };
 
-    constructor(private readonly fb: UntypedFormBuilder,
-                private _service: UserService,
-                private _alertService: AlertService,
-                private readonly activatedRoute: ActivatedRoute,
-                private _router: Router,
-                private _translate: TranslateService) {
-    }
+      grecaptcha.ready(() => {
+        grecaptcha.execute(environment.receptcha.sitekey, {action: 'RECOVER_PASSWORD'}).then((token) => {
+          this._service.recoverPasswordUser(token, recoverPassword)
+            .subscribe(() => {
+              const msg = this._translate.get('login.recover.password.change.email.valid');
+              msg.subscribe(message => this._alertService.success(message));
+              this.isSuccess = true;
 
-    ngOnInit(): void {
-        this.activatedRoute.params.subscribe(params => {
-            this.token = params?.['token'];
-            this.recoverPasswordId = params?.['recoverPasswordId'];
-        });
-    }
-
-    async onSubmit() {
-        if (this.form.valid) {
-            const recoverPassword = {
-                email: this.form.get('email').value,
-                token: this.token,
-                recoverPasswordId: this.recoverPasswordId,
-                newPassword: this.form.get('newPassword').value
-            };
-
-            grecaptcha.ready(() => {
-                grecaptcha.execute(environment.receptcha.sitekey, {action: 'RECOVER_PASSWORD'}).then((token) => {
-                    this._service.recoverPasswordUser(token, recoverPassword)
-                    .subscribe(() => {
-                        const msg = this._translate.get('login.recover.password.change.email.valid');
-                        msg.subscribe(message => this._alertService.success(message));
-                        this.isSuccess = true;
-
-                        setTimeout(() => {
-                            this._router.navigate(['/login']);
-                        }, 3000);
-                    });
-                });
+              setTimeout(() => {
+                this._router.navigate(['/login']);
+              }, 3000);
             });
-        }
+        });
+      });
+    }
+  }
+
+  passwordIsMatch(control: UntypedFormGroup) {
+    const password = control.get('newPassword');
+    const confirmPassword = control.get('confirmNewPassword');
+
+    if (!confirmPassword?.value) {
+      return;
     }
 
-    passwordIsMatch(control: UntypedFormGroup) {
-        const password = control.get('newPassword');
-        const confirmPassword = control.get('confirmNewPassword');
-
-        if (!confirmPassword?.value) {
-            return;
-        }
-
-        if (password?.value !== confirmPassword?.value) {
-            control.get('confirmNewPassword').setErrors({passwordNotMatch: true});
-        }
-
-        if (password?.value === confirmPassword?.value) {
-            control.get('confirmNewPassword').setErrors(null);
-        }
+    if (password?.value !== confirmPassword?.value) {
+      control.get('confirmNewPassword').setErrors({passwordNotMatch: true});
     }
+
+    if (password?.value === confirmPassword?.value) {
+      control.get('confirmNewPassword').setErrors(null);
+    }
+  }
 
 }
--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -7,104 +7,108 @@ import {environment} from '../../../environments/environment';
 import {Language} from '../../model/sk/Language';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
 
-    private _api = '/api/v1/user';
+  private _api = '/api/v1/user';
 
-    constructor(private readonly _http: HttpClient) {
+  constructor(private readonly _http: HttpClient) {
+  }
+
+  list(params: UserParams): Observable<Page<User>> {
+    return this._http.get<Page<User>>(`${environment.buddy}${this._api}`, {params: this.toHttpParams(params)});
+  }
+
+  create(user: User) {
+    return firstValueFrom(this._http.post(`${environment.buddy}${this._api}`, user));
+  }
+
+  update(user: User) {
+    return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${user.id}`, user));
+  }
+
+  activate(id: string) {
+    return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${id}/activate`, {}));
+  }
+
+  inactivate(id: string) {
+    return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${id}/inactivate`, {}));
+  }
+
+  login(token: string, login: { email: string, password: string }): Observable<HttpResponse<LoggedUser>> {
+    const headers = new HttpHeaders({
+      'g-recaptcha-response': token
+    });
+
+    return this._http.post<LoggedUser>(`${environment.buddy}${this._api}/login`, login, {
+      headers,
+      observe: 'response'
+    });
+  }
+
+  refreshToken(token: string): Observable<HttpResponse<void>> {
+    return this._http.post<void>(
+      `${environment.buddy}${this._api}/refresh-token`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        observe: 'response'
+      }
+    );
+  }
+
+  personalData(): Observable<HttpResponse<User>> {
+    return this._http.get<User>(`${environment.buddy}${this._api}/personal-data`, {observe: 'response'});
+  }
+
+  changePassword(changePassword: { currentPassword: string, newPassword: string }) {
+    return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/change-password`, changePassword));
+  }
+
+  changeLanguage(changeLanguage: { language: Language }) {
+    return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/change-language`, changeLanguage));
+  }
+
+  recoverPassword(token: string, recoverPassword: { email: string }) {
+    const headers = new HttpHeaders({
+      'g-recaptcha-response': token
+    });
+
+    return this._http.post(`${environment.buddy}${this._api}/recover-password`, recoverPassword, {headers});
+  }
+
+  recoverPasswordUser(token: string, recoverPassword: {
+    token: string,
+    recoverPasswordId: string,
+    newPassword: string
+  }) {
+    const headers = new HttpHeaders({
+      'g-recaptcha-response': token
+    });
+
+    return this._http.patch(`${environment.buddy}${this._api}/recover-password/change-password`, recoverPassword, {headers});
+  }
+
+  private toHttpParams(param?: UserParams): HttpParams {
+    let imutebleParams = new HttpParams();
+
+    if (param?.size) {
+      imutebleParams = imutebleParams.append('size', param.size);
+    }
+    if (param?.page) {
+      imutebleParams = imutebleParams.append('page', param.page);
+    }
+    if (param?.sort) {
+      imutebleParams = imutebleParams.append('sort', param.sort);
+    }
+    if (param?.search) {
+      imutebleParams = imutebleParams.append('search', param.search);
     }
 
-    list(params: UserParams): Observable<Page<User>> {
-        return this._http.get<Page<User>>(`${environment.buddy}${this._api}`, {params: this.toHttpParams(params)});
-    }
-
-    create(user: User) {
-        return firstValueFrom(this._http.post(`${environment.buddy}${this._api}`, user));
-    }
-
-    update(user: User) {
-        return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${user.id}`, user));
-    }
-
-    activate(id: string) {
-        return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${id}/activate`, {}));
-    }
-
-    inactivate(id: string) {
-        return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/${id}/inactivate`, {}));
-    }
-
-    login(token: string, login: { email: string, password: string }): Observable<HttpResponse<LoggedUser>> {
-        const headers = new HttpHeaders({
-            'g-recaptcha-response': token
-        });
-
-        return this._http.post<LoggedUser>(`${environment.buddy}${this._api}/login`, login, {
-            headers,
-            observe: 'response'
-        });
-    }
-
-    refreshToken(token: string): Observable<HttpResponse<void>> {
-        return this._http.post<void>(
-            `${environment.buddy}${this._api}/refresh-token`,
-            {},
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                observe: 'response'
-            }
-        );
-    }
-
-    personalData(): Observable<HttpResponse<User>> {
-        return this._http.get<User>(`${environment.buddy}${this._api}/personal-data`, {observe: 'response'});
-    }
-
-    changePassword(changePassword: { currentPassword: string, newPassword: string }) {
-        return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/change-password`, changePassword));
-    }
-
-    changeLanguage(changeLanguage: { language: Language }) {
-        return firstValueFrom(this._http.patch(`${environment.buddy}${this._api}/change-language`, changeLanguage));
-    }
-
-    recoverPassword(token: string, recoverPassword: { email: string }) {
-        const headers = new HttpHeaders({
-            'g-recaptcha-response': token
-        });
-
-        return this._http.post(`${environment.buddy}${this._api}/recover-password`, recoverPassword, {headers});
-    }
-
-    recoverPasswordUser(token: string, recoverPassword: { email: string, token: string, recoverPasswordId: string, newPassword: string }) {
-        const headers = new HttpHeaders({
-            'g-recaptcha-response': token
-        });
-
-        return this._http.patch(`${environment.buddy}${this._api}/recover-password/change-password`, recoverPassword, {headers});
-    }
-
-    private toHttpParams(param?: UserParams): HttpParams {
-        let imutebleParams = new HttpParams();
-
-        if (param?.size) {
-            imutebleParams = imutebleParams.append('size', param.size);
-        }
-        if (param?.page) {
-            imutebleParams = imutebleParams.append('page', param.page);
-        }
-        if (param?.sort) {
-            imutebleParams = imutebleParams.append('sort', param.sort);
-        }
-        if (param?.search) {
-            imutebleParams = imutebleParams.append('search', param.search);
-        }
-
-        return imutebleParams;
-    }
+    return imutebleParams;
+  }
 
 }
