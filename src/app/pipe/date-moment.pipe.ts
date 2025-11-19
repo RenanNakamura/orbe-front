0a53f1f6700bf28b97790a0b18c641006d49e7ff
RenanNakamura <nakamura.renan.j@gmail.com>
2025-11-19 16:12:31 -0300

    - Implementando reafatorando chat.

--- src/app/pipe/date-moment.pipe.ts
+++ src/app/pipe/date-moment.pipe.ts
@@ -1,5 +1,6 @@
 import {NgModule, Pipe, PipeTransform} from '@angular/core';
 import moment from 'moment-timezone';
+import {TranslateService} from "@ngx-translate/core";
 
 @Pipe({
   name: 'dateMoment',
@@ -26,12 +27,24 @@ export class DateMomentPipe implements PipeTransform {
 })
 export class DateOnlyHourPipe implements PipeTransform {
 
+  constructor(private _translate: TranslateService) {
+  }
+
   transform(value: any | null): any {
     if (!value) return '';
 
+    const now = moment().tz(moment.tz.guess());
     const date = moment(value).tz(moment.tz.guess());
 
-    return date.format('HH:mm');
+    if (date.isSame(now, 'day')) {
+      return date.format('HH:mm');
+    }
+
+    if (date.isSame(now.clone().subtract(1, 'day'), 'day')) {
+      return `${this._translate.instant('yesterday')} - ${date.format('HH:mm')}`;
+    }
+
+    return date.format('DD/MM/YYYY - HH:mm');
   }
 }
 
